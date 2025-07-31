69db8b9e03ed02e31e8d366020c81015f4fa1a6d
Anisha2622 <anishapawar0926@gmail.com>
2025-07-31 18:53:20 +0530

    Python Strings

---
+++ String/string.py
@@ -0,0 +1,19 @@
+name = "Anisha"
+friend = "Siddhi"
+anotherfriend = "Sakshi"
+apple = '''He said,
+Hi Anisha
+hey i am good
+i want eat an apple'''
+print("Hello,"+name)
+# print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+# print(name[6])  in 6th indexing there is no string so its show error
+print("Lets use a for loop\n") #helping string char print one by one 
+for character in apple:
+    print(character)
