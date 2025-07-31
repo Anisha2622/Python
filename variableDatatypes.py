9f60f998f1a4276fc8bac8c7c646f8b55b8a6b4d
Anisha2622 <anishapawar0926@gmail.com>
2025-07-31 15:33:59 +0530

    Variables and Datatypes

---
+++ variableDatatypes.py
@@ -0,0 +1,13 @@
+a = 123654789953
+print(a)
+c = 9
+b = "Anisha"
+print(b)
+print(a+c)
+d= True
+e = complex(8,2)
+print("The type of a is",type(a))
+print("The type of b is",type(b))
+print("The type of c is",type(c))
+print("The type of d is",type(d))
+print("The type of e is",type(e))
