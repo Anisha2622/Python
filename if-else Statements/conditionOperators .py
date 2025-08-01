4eebc9cb48f84c76d229fcc47d9e25b07bb9bbfa
Anisha2622 <anishapawar0926@gmail.com>
2025-08-01 23:52:13 +0530

    Condition Operators

---
+++ if-else Statements/conditionOperators .py
@@ -0,0 +1,14 @@
+a = int(input("Enter yor age"))
+print("Your age is:",a)
+
+#condition operators
+#>, <, >=, ==, <=, !=
+# print(a>18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+
+if(a>18):
+    print("You can drive")
+else:
+    print("You can't drive")
