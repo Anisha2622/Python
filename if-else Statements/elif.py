349cd2202fce85b8dc2f3a2e474acce704275178
Anisha2622 <anishapawar0926@gmail.com>
2025-08-02 00:08:53 +0530

    elif

---
+++ if-else Statements/elif.py
@@ -0,0 +1,23 @@
+num = int(input("Enter the value of num:"))
+if num < 0:
+    print("Number is negative")
+elif (num == 0):
+    print("Number is zero")
+elif (num == 999):
+    print("Number is special")
+else:
+    print("Number is positive")
+
+print("I am happy now")
+
+
+# applePrice = 10
+# budget = 200
+# if (budget - applePrice) > 50:
+#     print("Alexa, add 1 kg Apples to the cart")
+# elif(budget - applePrice) > 70:
+#     print("Its okay you can buy")
+# else:
+#     print("Alexa, do not add Apples to the cart")
+
+
