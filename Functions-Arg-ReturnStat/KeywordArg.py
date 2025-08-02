7e2882899a2537ecc35c6f9d5ae75c93a544d4b3
Anisha2622 <anishapawar0926@gmail.com>
2025-08-02 13:59:00 +0530

    Keyword Arguments

---
+++ Functions-Arg-ReturnStat/KeywordArg.py
@@ -0,0 +1,8 @@
+# Keyword arguments
+def average(a=9,b=1):
+    print("The average is ",(a+b)/2)
+#average(4,6)
+average(b=9)
+
+average (a=9,b=21) # This will print the average 
+# logic of keyword arguments is that we can call the function with any keyword we want 
