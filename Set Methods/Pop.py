6a171aad77c15c2bcc0ecd56efd1d64dd70b1c2a
Anisha2622 <anishapawar0926@gmail.com>
2025-08-03 13:43:02 +0530

    Set Methods

---
+++ Set Methods/Pop.py
@@ -0,0 +1,4 @@
+cities = {'New York', 'Los Angeles', 'Chicago2'}
+item = cities.pop()
+print(cities) 
+print (item)
---
+++ Set Methods/RemoveAndDiscard.py
@@ -0,0 +1,5 @@
+cities = {'New York', 'Los Angeles', 'Chicago2'}
+# cities.remove("New York")
+cities.discard("New York")
+
+print(cities)  
