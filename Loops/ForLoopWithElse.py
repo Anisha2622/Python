6b73390cb126baf494d99b1ced65f78eb88c2fed
Anisha2622 <anishapawar0926@gmail.com>
2025-08-03 16:44:21 +0530

    For Loop with Else

---
+++ Loops/ForLoopWithElse.py
@@ -0,0 +1,16 @@
+for i in range(6):
+    print(i)
+    if i == 4:
+        break
+
+else:
+    print("The loop finished normally")
+
+# loop with a condition
+i = 0
+while i < 7:
+    print(i)
+    i = i + 1
+
+else:
+    print("The loop finished normally")
