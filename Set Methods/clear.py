043901a21f0815ae76393afd0267499449a47675
Anisha2622 <anishapawar0926@gmail.com>
2025-08-03 13:47:06 +0530

    Set Methods

---
+++ Set Methods/clear.py
@@ -0,0 +1,6 @@
+cities = {'New York', 'Los Angeles', 'Chicago2'}
+cities.clear()
+print(cities)
+#  Output: set()  # The set is now empty.
+#  Note: The clear() method does not return a value, so it does not print anything.
+#  However, the set is now empty, as shown by the print statement.
