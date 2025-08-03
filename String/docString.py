4161b54ed37df417c9bfff1aa9d2e0d51a7bed90
Anisha2622 <anishapawar0926@gmail.com>
2025-08-03 12:00:54 +0530

    docString in Python

---
+++ String/docString.py
@@ -0,0 +1,12 @@
+# docstring program
+
+def square(n): 
+    '''Return the square of a number.'''# docstring
+    print(n ** 2)
+square(5)
+print(square.__doc__)
+
+# used for docstrings in functions
+# used for docstrings in classes
+# used for docstrings in modules
+# used for document of our code 
