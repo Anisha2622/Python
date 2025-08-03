19721dd678cf87b8465f35b503a19a8a720b5a0a
Anisha2622 <anishapawar0926@gmail.com>
2025-08-03 13:45:12 +0530

    Set Methods

---
+++ Set Methods/Del.py
@@ -0,0 +1,5 @@
+cities = {'New York', 'Los Angeles', 'Chicago2'}
+del cities
+print(cities)
+# Output: NameError: name 'cities' is not defined
+#  The variable 'cities' has been deleted, so it can't be printed.
