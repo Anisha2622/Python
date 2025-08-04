009b09c62d126039931c2d6a3ee379247a007406
Anisha2622 <anishapawar0926@gmail.com>
2025-08-04 23:30:58 +0530

    Short-hand-if-else

---
+++ if-else-Statements/Short-hand-IfElse.py
@@ -0,0 +1,14 @@
+a = 330
+b = 3303
+print("A") if a > b else print("=") if a == b else print("B")
+ # short hand if else is used here
+
+c = 9 if a>b else 0 # this is a short hand if else
+print(c)
+
+
+
+# short hand if ekse it use because it is more readable and more pythonic
+# it is used when we have to assign a value to a variable based on a condition
+
+
