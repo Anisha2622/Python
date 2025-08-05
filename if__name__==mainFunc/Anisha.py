56d89fe17fe6b02aa0641870158c051b4f509a97
Anisha2622 <anishapawar0926@gmail.com>
2025-08-05 18:25:11 +0530

    if __name__ == "__main__" in python

---
+++ if__name__==mainFunc/Anisha.py
@@ -0,0 +1,8 @@
+def welcome():
+    print("Hey you are welcome from Anisha")
+
+# code to be run when the script is run directly 
+print(__name__) 
+if __name__ == "__main__": # run directly or being imported as a module into another script 
+
+    welcome()
---
+++ if__name__==mainFunc/main.py
@@ -0,0 +1,3 @@
+import Anisha
+
+Anisha.welcome()
