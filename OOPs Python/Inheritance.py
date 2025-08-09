0b843e9cab2f17d470dc5b739c6d6197546c6d4f
Anisha2622 <anishapawar0926@gmail.com>
2025-08-09 23:45:21 +0530

    Inheritance

---
+++ OOPs Python/Inheritance.py
@@ -0,0 +1,17 @@
+# Inheritance in Python 
+# Inheritance is a mechanism in object-oriented programming (OOP) that allows one class to inherit
+# properties and methods from another class. The class that is being inherited from is called 
+# the parent class or the superclass, and the class that is doing the inheriting is called 
+# the child class or the subclass.
+class  employee:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+    def showDetails(self):
+        print(f"The name of employee: {self.id} is {self.name}")
+
+class Programmer(Employee):
+    def showLanguage(self):
+        print("The default language of programmer is Python")
+        
